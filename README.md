eefe953acc9b8cfd9ff9f3d80ce5056744d1af66
MarynaPR <pryadkamaryna@gmail.com>
2020-10-09 22:27:55 -0500

    function modification to store input

--- README.md
+++ README.md
@@ -19,3 +19,89 @@ Video Resources on youtube:
 
 <!-- https://youtu.be/S-gMLzlG_ME -->
 <!-- https://youtu.be/4UoUqnjUC2c -->
+
+Student submitted deployed live application URL
+True
+False
+Application loads at deployed URL (no 404, or Heroku/GH Pages error)
+True
+False
+Student submitted application GitHub URL
+True
+False
+GitHub Repo contains application code
+True
+False
+Technical Acceptance Criteria
+Keep your browser console open and watch out for errors from this point forward.
+User can search for weather reports by city using the openweathermap API
+True
+False
+After searching for a city, the following information is displayed: city name, date, an icon representation of weather conditions, current temperature, current humidity, windspeed, uv index, and a 5 day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+True
+False
+UV index uses different colors to indicate whether the conditions are favorable, moderate, or severe
+True
+False
+Application stores previously searched for cities in local storage and displays them to the user
+True
+False
+User is able to repeat a search for a city's current and future weather conditions by clicking on it in the search history
+True
+False
+Repository Quality
+Repository has a unique name
+True
+False
+Repository follows best practices for file structure and naming conventions
+True
+False
+Repository follows best practices for variable and function naming conventions, indentation, quality comments, etc.
+True
+False
+Repository contains multiple descriptive commit messages
+True
+False
+Repository contains quality README with description, screenshot, link to deployed application
+True
+False
+Application Quality
+Application user experience is intuitive and easy to navigate
+True
+False
+Application user interface style is clean and polished
+True
+False
+Application resembles the mock-up functionality provided in Challenge instructions
+True
+False
+Academic Integrity
+If any of of the following are true, partner with your instructor to investigate plagiarism. Contact your SSM if you believe plagiarism has occurred.
+Application contains a low number of commits
+True
+False
+Application codebase appears advanced for the student, at this point in the course, or in general
+True
+False
+Application package.json file references a different author and/or repository
+True
+False
+Application package.json file references much older versions of npm packages
+True
+False
+* Application isn't deployed
+* Missing GitHub submission URL
+* GitHub repo is empty
+* User can't search for weather reports using the openweathermap API
+* Missing search result information outlined in the Challenge instructions
+* UV index does not use different colors to indicate whether the conditions are favorable, moderate, or severe
+* Application doesn't store or display previously searched cities for the user
+* User is unable to repeat a search for a city's current and future weather conditions by clicking on it in the search history
+* Repo doesn't have a unique name
+* Code doesn't follow recommended file/folder structure
+* Code doesn't follow good practices for variable names, indentation, lacks quality comments
+* Repo doesn't contain multiple descriptive commit messages
+* Repo doesn't include a high quality README with screenshots, a description, link to deployed application
+* Application user experience could be more intuitive
+* Application user interface needs work
+* Application could resemble mock-up functionality provided with Challenge instructions more closely
--- weatherscript.js
+++ weatherscript.js
@@ -1,89 +1,118 @@
-//     "http://api.openweathermap.org/data/2.5/weather?q="
-//     + searchCity
-//     //Madison
-//     + "&appid="
-//     //8d27861de0e9d9289a864a13d6ed84d0
-//     + apiKeys
-//     + "units=imperial"
-//GIVEN a weather dashboard with form inputs WHEN I search for a city THEN I am presented with current and future conditions for that city and that city is added to the search history
-var apiKeys = "8d27861de0e9d9289a864a13d6ed84d0";
-var cities = [];
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+// WHEN I click on a city in the search history THEN I am again presented with current and future conditions for that city
+
+//var calendar = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+//var cities = [];
 var responseContainerEl = document.querySelector("#weatherCall");
-var fiveDayForecast = document.querySelector("#fiveDayContainer");
 var fiveDayForecast = document.querySelector("#fiveDayContainer");
 //fuction to input city
 function searchFavCity() {
-    console.log("show")
-    // event.preventDefault()
-    var searchCity = document.querySelector("#searchCity").value.trim();
-    // weatherCall(searchCity);
-    //  citiesSearched(searchCity);
-    var apiCall = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=8d27861de0e9d9289a864a13d6ed84d0";
+    console.log("show");
+    var searchCity = document.querySelector("#searchCity").value;
+    weatherCall(searchCity);
+    storeFavCity(searchCity);
+    //condition if no input
     if (searchCity) {
         getForecast(searchCity);
-        citiesSearched();
-        storeCitiesSearched(searchCity);
+        storeFavCity(searchCity);
     }
     else {
         alert("Select a City");
     }
-    //document.querySelector("#citySearched").value = "";
 }
-searchFavCity();
+
 // store cities searched
-function storeFavCity() {
+function storeFavCity(searchCity) {
     console.log("city");
 
-    // loop
-    for (var i = 0; i < cities.length; i++) {
-        //buttons for each city in the array
-        var listOfCities = $("<button>");
-        //class
-        listOfCities.addClass("btn");
-        listOfCities.attr("searchCity", cities[i]);
-        listOfCities.text(cities[i]);
-        // add button
-        $("#list-group").append(listOfCities);
+    //var buttonEl =document.querySelector("#searchCity");
+    var listOfCities = document.createElement("li");
+    listOfCities.classList.add("list-group-item", "list-group-item-action");
+    var string = searchCity;
+    listOfCities.textContent = string;
+    var storedCities = document.querySelector(".storedFavCity");
+    //console.log(event.this)
+    storedCities.onclick = function () {
+        // console.log(event.this.city)
+        if (event.this.city == "li") {
+            weatherCall(event.this.textContent);
+        }
     }
-}
+    storedCities.appendChild(listOfCities);
+};
+// module4 info:
+// var buttonEl = document.querySelector("#save-task");
+// var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-//click the button on city name
-$("#searchCity").on("click", function (event) {
-    event.preventDefault();
-    //add entered input
-    var city = $("#citySearch").value().trim();
-    //add city to array
-    cities.push(city);
-})
+// var createTaskHandler = function() {
+//   var listItemEl = document.createElement("li");
+//   listItemEl.className = "task-item";
+//   listItemEl.textContent = "This is a new task.";
+//   tasksToDoEl.appendChild(listItemEl);
+// };
 
-storeFavCity();
-$(document).on("click", ".searchBtn", searchFavCity);
+//buttonEl.addEventListener("click", createTaskHandler);
+
+
+// // loop
+// for (var i = 0; i < searchCity.length; i++) {
+//     //buttons for each city in the array
+//     var listOfCities = $("<button>");
+//     //class
+//     listOfCities.addClass("btn");
+//     listOfCities.attr("searchCity", searchCity[i]);
+//     listOfCities.text(searchCity[i]);
+//     // add button
+//     $("#list-group").append(listOfCities);
+// }
+
+// //click the button on city name
+// $("#searchBtn").on("click", function (event) {
+//     event.preventDefault();
+//     console.log("test");
+//     //add entered input
+//     console.log($("#searchCity"));
+//     var searchCity = $("#searchCity").val().trim();
+//     //add city to array
+//     cities.push(searchCity);
+//     //conditional for once pick the city show the weather for it?
+// })
+
+//storeFavCity();
+//$(document).on("click", ".searchBtn", searchFavCity);
 //document.querySelector("#searchBtn").addEventListener("click", searchFavCity);
 // var cities = JSON.parse(localStorage.getItem("cityStorage")) || [];
 
-
 //function for weather for today
-function weatherCall() {
-    //console.log("get the weather!")
+function weatherCall(searchCity) {
+
+    console.log("get the weather!")
     fetch(
-        "http://api.openweathermap.org/data/2.5/weather?q="
+        "http://api.openweathermap.org/data/2.5/forecast?q="
         + searchCity
-        + "&appid=8d27861de0e9d9289a864a13d6ed84d0&units=imperial"
+        + "&appid=b76c30386bab576d023d70f50d7d35cb"
     )
         .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            var searchCity = response.querySelector[0].title;
+        .then(function (response) {
+            // var searchCity = response.querySelector[0].title;
             //display
             var temperature = response.list[i].main.temp//main.humidity//
             var dateYYYYMMDDHH = response.list[i].dt - text
-            var calendar = moment(dateYYYYMMDDHH).format("dddd, MMMM Do YYYY, h:mm:ss a")
+            //var calendar = moment(dateYYYYMMDDHH).format("dddd, MMMM Do YYYY, h:mm:ss a")
             var humidity = response.list[i].weather[0].main
             var emoji = response.list[i].weather[0].main
 
             responseContainerEl.innerHTML = "<h2>" + searchCity + "</h2>";
-            fiveDayForecast.innerHTML = "";
+            //fiveDayForecast.innerHTML = "";
+            fiveDayForecast.innerHTML = "<h3 class=\"mt-3\">5-Day Forecast:</h3>";
+            fiveDayForecastRow = document.createElement("div");
+            fiveDayForecastRow.name = "\"row\"";
             //emoji
             if (emoji == "Clouds") {
                 emoji = "☁️"
@@ -98,19 +127,18 @@ function weatherCall() {
             } else if (emoji == "Clear") {
                 emoji = "☀️"
             }
+            getForecast(searchCity);
         })
 
 }
 //weather forecast
 function getForecast(searchCity) {
+    console.log(searchCity);
     //fetch request
     fetch(
-        "http://api.openweathermap.org/data/2.5/weather?q="
+        "http://api.openweathermap.org/data/2.5/forecast?q="
         + searchCity
-        + "&appid="
-        //8d27861de0e9d9289a864a13d6ed84d0
-        + apiKeys
-        + "units=imperial"
+        + "&appid=b76c30386bab576d023d70f50d7d35cb"
     )
         //convert the response to JSON
         .then(function (response) {
@@ -119,15 +147,15 @@ function getForecast(searchCity) {
         .then(function (response) {
             console.log("getForecast", response);
 
-            fiveDayForecast.innerHTML = "<h3 class=\"mt-3\">5-Day Forecast:</h3>";
-            fiveDayForecastRow = document.createElement("div");
-            fiveDayForecastRow.name = "\"row\"";
-            //
+            // fiveDayForecast.innerHTML = "<h3 class=\"mt-3\">5-Day Forecast:</h3>";
+            // fiveDayForecastRow = document.createElement("div");
+            // fiveDayForecastRow.name = "\"row\"";
+            //loop
             for (var i = 0; i < response.list.length; i += 5) {
                 var temperature = response.list[i].main.temp//main.humidity//
                 var dateYYYYMMDDHH = response.list[i].dt - text
                 var calendar = moment(dateYYYYMMDDHH).format("dddd, MMMM Do YYYY, h:mm:ss a")
-                var humidity = response.list[i].weather[0].main
+                //    var humidity = response.list[i].weather[0].main
                 var emoji = response.list[i].weather[0].main
                 //emoji
                 if (emoji == "Clouds") {
@@ -140,35 +168,12 @@ function getForecast(searchCity) {
                     emoji = "🌧"
                 } else if (emoji == "Snow") {
                     emoji = "❄️"
-                } else if (emoji == "Clear") {
-                    emoji = "☀️"
-                }
-
+                } else if (emoji == "Clear") { emoji = "☀️" }
                 responseContainerEl.innerHTML = "";
-
-
             }
         })
 }
-
+//fiveDayForecast.appendChild()
 document.querySelector("#searchBtn").addEventListener("click", searchFavCity);
-
-
-//     $(".weatherResponse").html("");//clear
-//     $.getJSON(apiCall, weatherCallback);
-//     //function to call the data
-//     //var weatherCallback = function (weatherData) {
-//     function weatherCallback(weatherData) {
-//         // variables for the description
-//         var cityName = weatherData.name;
-//         var country = weatherData.sys.country;
-//         var description = weatherData.weather[0].description;
-//         //class =weatherResponse
-//         //$(".weatherResponse").append(cityName, country, description);
-//         $(".weatherResponse").append("The weathr in " + cityName + " " + country + " is currently "
-//             + description);
-//         //console.log(weatherData.name,
-//         //   weatherData.sys.country,
-//         //  weatherData.weather[0].description);
 
 storeFavCity();
